6a66f8baabda02ae4a488bdbce850a21fe8faacb
Michael Carmelino Dueñas <103915303+stxfxno@users.noreply.github.com>
2024-04-09 19:37:45 -0500

    Add files via upload

---
+++ Aplicaciones Web/Semana 2/DemoRESTWS-master/DemoRESTWS/Models/Customer.cs
@@ -0,0 +1,24 @@
+﻿namespace DemoRESTWS.Models
+{
+    public class Customer
+    {
+        public int CustomerId { get; }
+        public string FirstName { get; }
+        public string LastName { get; }
+        public string DNI { get; }
+        public DateOnly BirthDate { get; }
+
+        public Customer(int _CustomerId, string _FirstName, string _LastName, string _DNI, DateOnly BirthDay)
+        {
+            CustomerId = _CustomerId;
+            FirstName = _FirstName;
+            LastName = _LastName;
+            DNI = _DNI;
+            BirthDate = BirthDay;
+        }
+
+        public bool IsBirthDate(DateOnly today) {
+            return today == BirthDate;
+        }
+    }
+}
